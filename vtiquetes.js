face3fcc24438737af70c575da387230d550e59d
alexdz <RonyDiaz98@outlook.es>
2022-02-08 23:15:40 -0500

    ya no se repiten los destinos

--- vtiquetes.js
+++ vtiquetes.js
@@ -12,8 +12,10 @@ class Transporte
 }
 
 var transportes = [];
-transportes.push(new Transporte(1, "avion", "Ecuadorrrr", "Canada", "7 am", 45))
-transportes.push(new Transporte(1, "bus", "quito", "guayaquil", "12 am", 35))
+transportes.push(new Transporte(1, "avion", "Ecuadorrrr", "Canada", "7 am", 45));
+transportes.push(new Transporte(1, "bus", "quito", "guayaquil", "12 am", 35));
+transportes.push(new Transporte(2, "avion", "Ecuadorrr", "Canada", "4 pm", 45));
+transportes.push(new Transporte(2, "bus", "Quito", "Manabi", "3 pm", 35));
 
 
 //elecciones del usuario
@@ -25,16 +27,24 @@ var pasajeros_cantidad;
 var s_destiny = true;
 var s_hour = false;
 var c_people = false;
+var n_d = 0;
+var n_h = 0;
+var n_p = 0;
+var c_destiny = transportes.length - 1;
+var destinos = [];
+var aceptar;
 
 var b_back = document.getElementById("back");
 var b_select = document.getElementById("select");
 var b_next = document.getElementById("next");
+var display = document.getElementById("pantalla");
 
 b_back.addEventListener("click", go_back);
 b_select.addEventListener("click", select_item);
 b_next.addEventListener("click", go_next);
 
-
+clasificar_destinos();
+var c_destinos = destinos.length - 1;
 
 //FUNCTIONS//
 
@@ -45,7 +55,14 @@ function go_back()
     switch(true)
     {
         case s_destiny:
-            alert("destino");
+            if(n_d > 0 && n_d <= c_destinos){
+                n_d -= 1;
+                display.innerHTML = destinos[n_d];
+            }
+            else{
+                n_d = c_destinos;
+                display.innerHTML = destinos[n_d];    
+            }
         break;
         case s_hour:
             alert("hora");
@@ -60,7 +77,10 @@ function select_item()
     switch(true)
     {
         case s_destiny:
-            alert("destino");
+            dest_usuario = transportes[n_d].destiny;
+            display.innerHTML = "seleccione hora";
+            s_destiny = false;
+            s_hour = true;
         break;
         case s_hour:
             alert("hora");
@@ -76,10 +96,25 @@ function go_next()
      switch(true)
     {
         case s_destiny:
-            alert("destino");
+            if(n_d >= 0 && n_d < c_destinos)
+            {
+                n_d++;
+                display.innerHTML = destinos[n_d];
+            }
+            else{
+                n_d = 0;
+                display.innerHTML = destinos[n_d];
+            }
         break;
         case s_hour:
-            alert("hora");
+            if(n_h >= 0 && n_h < c_destiny){
+                n_h++;
+                display.innerHTML = transportes[n_d].hour;
+            }
+            else{
+                n_h = 0;
+                display.innerHTML = transportes[n_d].hour;
+            }
         break;
         case c_people:
             alert("pasajeros")
@@ -87,6 +122,30 @@ function go_next()
     }
 }
 
+// funcion de clasificacion
+function clasificar_destinos()
+{
+    for(n in transportes)
+{
+    if(n == 0)
+    {
+        destinos.push(transportes[n].destiny);
+    }
+    if(n > 0 && n <= c_destiny){
+        aceptar = true;
+        for(d in destinos){
+            if(destinos[d] == transportes[n].destiny){
+                aceptar = false;
+            }
+        }
+        if(aceptar)
+        {
+            destinos.push(transportes[n].destiny);
+        }
+    }
+
+}
+}
 //funciones del proceso
 function peticion_viaje()
 {
